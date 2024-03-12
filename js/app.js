bdc07e52977bd1362406eb9153ec4258e333851d
Cigno05 <paolocalcagni.82@gmail.com>
2024-03-12 17:28:30 +0100

    Palindrome word

--- js/app.js
+++ js/app.js
@@ -2,26 +2,50 @@ console.log('JS funziona');
 
 // Chiedere all’utente di inserire una parola
 
-// const userWord = prompt('inserisci una parola e vediamo se è palindroma');
+let userWord = prompt('inserisci una parola e vediamo se è palindroma');
 
 // Creare una funzione per capire se la parola inserita è palindroma
 
 // function isPalindromeWord(stringWord) {
 // }  
-
-let stringWord = 'ciao';
+let stringWord = userWord;
 let reverseStringWord = ''
-// Inverto la stringa con un ciclo for
-for ( let index = stringWord.length - 1; index >= 0; index--) {
-    // console.log(stringWord[index])
 
-    reverseStringWord += stringWord[index];
+// Inverto la stringa con un ciclo for
+
+function isPalindromeWord(word) {
+    
+    for ( let index = stringWord.length - 1; index >= 0; index--) {
+        // console.log(stringWord[index])
+        
+        reverseStringWord += stringWord[index];
+    }
+    console.log(reverseStringWord)
+
+    if (stringWord === reverseStringWord) {
+        return true
+    } else {
+        return false
+    }
+
 }
 
-console.log(reverseStringWord);
+const palindromeWord = isPalindromeWord(stringWord)
+console.log(palindromeWord)
 
-if (stringWord === reverseStringWord) {
-    
+let messageResult = document.getElementById('container')
+if (palindromeWord === true) {
+    messageResult.innerHTML = `
+    <div>
+        <h2>E' palindroma</h2>
+    </div>
+    `
+} else {
+    messageResult.innerHTML = `
+    <div>
+        <h2>Non è palindroma</h2>
+    </div>
+    `
 }
 
 
