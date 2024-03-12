499324c2509af15dd7dc11e8c428de06262b9690
Cigno05 <paolocalcagni.82@gmail.com>
2024-03-12 16:54:04 +0100

    Reverse word calculate

--- js/app.js
+++ js/app.js
@@ -1,1 +1,65 @@
 console.log('JS funziona');
+
+// Chiedere all’utente di inserire una parola
+
+// const userWord = prompt('inserisci una parola e vediamo se è palindroma');
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// function isPalindromeWord(stringWord) {
+// }  
+
+let stringWord = 'ciao';
+let reverseStringWord = ''
+// Inverto la stringa con un ciclo for
+for ( let index = stringWord.length - 1; index >= 0; index--) {
+    // console.log(stringWord[index])
+
+    reverseStringWord += stringWord[index];
+}
+
+console.log(reverseStringWord);
+
+if (stringWord === reverseStringWord) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
